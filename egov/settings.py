9d97fef31eb9bca05cd0ceef8a4606b7c2bfceba
lanfia <44394239+lanfia@users.noreply.github.com>
2018-12-01 09:10:39 +0000

    Update settings.py

--- egov/settings.py
+++ egov/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = ''(8@pd-ayfk%)2)x(%dy3gy&xanho6i)ck3(o0o@3*!ce6#r@r@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
